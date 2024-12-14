f61f17b4b132f1795502ca437cb2d697a32cadd7
MBURU <wawerumburu157@gmail.com>
2024-12-15 00:12:23 +0300

    changes

--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from bool_functions import return_true
-
-def test_return_true():
-    '''in bool_functions, function "return_true" returns True.'''
-    assert return_true() == True
+def return_true():
+    """Returns the boolean value True."""
+    return True
